5539899d36ed0d34657e9c471503b2bccaeb8a9d
viggorunsten <viggo.runsten@gmail.com>
2023-11-21 11:42:14 +0100

    Added SSH lib and some initial command code
    + dependecies in build gradle (module app)

--- VoiceCommandProject/app/src/main/java/com/example/voicecommandproject/MainActivity.java
+++ VoiceCommandProject/app/src/main/java/com/example/voicecommandproject/MainActivity.java
@@ -12,7 +12,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+
+import ch.ethz.ssh2.Connection;
+import ch.ethz.ssh2.Session;
+import ch.ethz.ssh2.StreamGobbler;
 
 class VoiceControlActivity extends AppCompatActivity {
 
@@ -107,7 +115,7 @@ class VoiceControlActivity extends AppCompatActivity {
 
     private void turnOnLamp() {
         // Code to turn on the lamp
-        statusTextView.setText("Turning on the lamp...");
+        statusTextView.setText("Turning on the lamp... ");
 
         // Add code from labs
 
@@ -115,7 +123,7 @@ class VoiceControlActivity extends AppCompatActivity {
 
     private void turnOffLamp() {
         // Code to turn off the lamp
-        statusTextView.setText("Turning off the lamp...");
+        statusTextView.setText("Turning off the lamp... ");
 
         // Add code from labs
     }
@@ -126,5 +134,39 @@ class VoiceControlActivity extends AppCompatActivity {
             speechRecognizer.destroy();
         }
         super.onDestroy();
+    }
+
+    public void run (String command) {
+        String hostname = "hostname";
+        String username = "username";
+        String password = "password";
+        try
+        {
+            Connection conn = new Connection(hostname); //init connection
+            conn.connect(); //start connection to the hostname
+            boolean isAuthenticated = conn.authenticateWithPassword(username,
+                    password);
+            if (isAuthenticated == false)
+                throw new IOException("Authentication failed.");
+            Session sess = conn.openSession();
+            sess.execCommand(command);
+            InputStream stdout = new StreamGobbler(sess.getStdout());
+            BufferedReader br = new BufferedReader(new InputStreamReader(stdout));
+
+            //reads text
+            while (true){
+                String line = br.readLine(); // read line
+                if (line == null)
+                    break;
+                System.out.println(line);
+            }
+            /* Show exit status, if available (otherwise "null") */
+            System.out.println("ExitCode: " + sess.getExitStatus());
+            sess.close(); // Close this session
+            conn.close();
+        }
+        catch (IOException e)
+        { e.printStackTrace(System.err);
+            System.exit(2); }
     }
 }
