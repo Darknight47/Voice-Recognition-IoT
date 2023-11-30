dc236cabb7aa3367b3b2f7326b661793255af6a6
viggorunsten <viggo.runsten@gmail.com>
2023-11-30 19:14:43 +0100

    Setup the NLP environment and a first UI
    NLP environment with Google cloud key and connections.
    UI with new colors and an animation

--- VoiceCommandProject/app/src/main/java/com/example/voicecommandproject/MainActivity.java
+++ VoiceCommandProject/app/src/main/java/com/example/voicecommandproject/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.voicecommandproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.content.ActivityNotFoundException;
 import android.os.Bundle;
 
 import android.speech.RecognitionListener;
@@ -10,20 +13,39 @@ import android.speech.SpeechRecognizer;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Locale;
 
 import ch.ethz.ssh2.Connection;
 import ch.ethz.ssh2.Session;
 import ch.ethz.ssh2.StreamGobbler;
 
+// Google Cloud NLP API
+import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.cloud.language.v1.Document;
+import com.google.cloud.language.v1.LanguageServiceClient;
+import com.google.cloud.language.v1.LanguageServiceSettings;
+import com.google.cloud.language.v1.AnalyzeEntitiesRequest;
+import com.google.cloud.language.v1.AnalyzeEntitiesResponse;
+import com.google.cloud.language.v1.Entity;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.language.v1.LanguageServiceClient;
+import com.google.cloud.language.v1.LanguageServiceSettings;
+
+
 public class MainActivity extends AppCompatActivity {
 
+    private int REQUEST_CODE_VOICE_RECOGNITION = 0;
     private SpeechRecognizer speechRecognizer;
 
     private TextView statusTextView;
@@ -35,8 +57,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_voice_control);
 
         statusTextView = findViewById(R.id.statusTextView);
-        startButton = findViewById(R.id.startButton);
+        startButton = findViewById(R.id.startVoiceRecognitionButton);
 
+        // Animations
+        ImageView speechAnimation = findViewById(R.id.speechAnimation);
+
+        ObjectAnimator scaleDown = ObjectAnimator.ofPropertyValuesHolder(
+                speechAnimation,
+                PropertyValuesHolder.ofFloat("scaleX", 1.2f),
+                PropertyValuesHolder.ofFloat("scaleY", 1.2f));
+        scaleDown.setDuration(310);
+        scaleDown.setRepeatCount(ObjectAnimator.INFINITE);
+        scaleDown.setRepeatMode(ObjectAnimator.REVERSE);
+        scaleDown.start();
+
+        // Google Cloud NLP API connections
+        /*
+        LanguageServiceSettings settings = LanguageServiceSettings.newBuilder()
+                .setCredentialsProvider(FixedCredentialsProvider.create(myCredentials))
+                .build();
+
+        try (LanguageServiceClient language = LanguageServiceClient.create(settings)) {
+            Document doc = Document.newBuilder().setContent(text).setType(Document.Type.PLAIN_TEXT).build();
+            AnalyzeEntitiesRequest request = AnalyzeEntitiesRequest.newBuilder()
+                    .setDocument(doc)
+                    .build();
+            AnalyzeEntitiesResponse response = language.analyzeEntities(request);
+
+            for (Entity entity : response.getEntitiesList()) {
+                System.out.printf("Entity: %s", entity.getName());
+            }
+        }
+        */
         speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
         speechRecognizer.setRecognitionListener(new RecognitionListener() {
 
@@ -105,6 +157,21 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void startVoiceRecognition() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak now...");
+
+        try {
+            startActivityForResult(intent, REQUEST_CODE_VOICE_RECOGNITION);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(), "Sorry, your device doesn't support speech input", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
     private void processCommand(String command) {
         if (command.toLowerCase().contains("turn on the lamp")) {
             turnOnLamp();
@@ -169,4 +236,28 @@ public class MainActivity extends AppCompatActivity {
         { e.printStackTrace(System.err);
             System.exit(2); }
     }
+
+
+    private void authenticateGoogleCloud() {
+        try {
+            // Load the service account key JSON file
+            InputStream serviceAccount = getResources().openRawResource(R.raw.authentication_key);
+
+            // Authenticate with the service account
+            GoogleCredentials credentials = ServiceAccountCredentials.fromStream(serviceAccount);
+
+            // Use the credentials to initialize the client
+            LanguageServiceSettings settings = LanguageServiceSettings.newBuilder()
+                    .setCredentialsProvider(FixedCredentialsProvider.create(credentials))
+                    .build();
+
+            LanguageServiceClient languageServiceClient = LanguageServiceClient.create(settings);
+
+            // Now you can use languageServiceClient to access the API
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
