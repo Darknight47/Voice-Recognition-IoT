c2b2609c4c25e365a593fd773efe8e6741efa56f
DarkNight <farhadgunners@gmail.com>
2023-12-04 09:05:13 +0100

    Updated import part

--- VoiceCommandProject/app/src/main/java/com/example/voicecommandproject/MainActivity.java
+++ VoiceCommandProject/app/src/main/java/com/example/voicecommandproject/MainActivity.java
@@ -4,17 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 
-import android.animation.PropertyValuesHolder;
 import android.content.ActivityNotFoundException;
 import android.os.Bundle;
 
 import android.speech.RecognizerIntent;
-import android.speech.SpeechRecognizer;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.util.Log;
 
 
 
