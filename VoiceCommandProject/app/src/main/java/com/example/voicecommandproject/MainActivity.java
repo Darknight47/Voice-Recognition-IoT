4c8fb0c4887711a09255b10c13e483ebbb063e66
DarkNight <farhadgunners@gmail.com>
2023-12-05 09:13:16 +0100

    Removed old imports

--- VoiceCommandProject/app/src/main/java/com/example/voicecommandproject/MainActivity.java
+++ VoiceCommandProject/app/src/main/java/com/example/voicecommandproject/MainActivity.java
@@ -1,19 +1,6 @@
 package com.example.voicecommandproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 
-import android.content.ActivityNotFoundException;
-import android.os.Bundle;
-
-import android.speech.RecognizerIntent;
-import android.content.Intent;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.util.Log;
 
 
 
