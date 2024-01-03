077fdaf274444c5c67696bd3bdbbf85c5284ad3c
viggorunsten <viggo.runsten@gmail.com>
2024-01-03 11:22:48 +0100

    Cleaning some old unused code

--- VoiceCommandProject/app/src/main/java/com/example/voicecommandproject/MainActivity.java
+++ VoiceCommandProject/app/src/main/java/com/example/voicecommandproject/MainActivity.java
@@ -23,23 +23,13 @@ import ch.ethz.ssh2.Connection;
 import ch.ethz.ssh2.Session;
 import ch.ethz.ssh2.StreamGobbler;
 
-// Google Cloud NLP API
-import com.google.api.client.util.Lists;
-import com.google.api.gax.core.FixedCredentialsProvider;
+// Google Cloud API
+
 import com.google.auth.oauth2.AccessToken;
-import com.google.cloud.language.v1.Document;
-import com.google.cloud.language.v1.LanguageServiceClient;
-import com.google.cloud.language.v1.LanguageServiceSettings;
-import com.google.cloud.language.v1.AnalyzeEntitiesRequest;
-import com.google.cloud.language.v1.AnalyzeEntitiesResponse;
-import com.google.cloud.language.v1.Entity;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.auth.oauth2.ServiceAccountCredentials;
-import com.google.cloud.language.v1.LanguageServiceClient;
-import com.google.cloud.language.v1.LanguageServiceSettings;
 
-import java.io.InputStream;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -49,9 +39,6 @@ import okhttp3.Response;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import okhttp3.*;
-import org.json.JSONObject;
-import java.io.IOException;
 import java.util.Collections;
 
 
@@ -87,24 +74,6 @@ public class MainActivity extends AppCompatActivity {
                 Log.e("Model Error", "Failed to send command", e);
             }
         });
-
-        // Google Cloud NLP API connections
-        /*
-        LanguageServiceSettings settings = LanguageServiceSettings.newBuilder()
-                .setCredentialsProvider(FixedCredentialsProvider.create(myCredentials))
-                .build();
-
-        try (LanguageServiceClient language = LanguageServiceClient.create(settings)) {
-            Document doc = Document.newBuilder().setContent(text).setType(Document.Type.PLAIN_TEXT).build();
-            AnalyzeEntitiesRequest request = AnalyzeEntitiesRequest.newBuilder()
-                    .setDocument(doc)
-                    .build();
-            AnalyzeEntitiesResponse response = language.analyzeEntities(request);
-
-            for (Entity entity : response.getEntitiesList()) {
-                System.out.printf("Entity: %s", entity.getName());
-            }
-        }*/
     }
 
     public interface ModelResponseCallback {
@@ -205,29 +174,6 @@ public class MainActivity extends AppCompatActivity {
             System.exit(2); }
     }
 
-    /*
-    private void authenticateGoogleCloud() {
-        try {
-            // Load the service account key JSON file
-            InputStream serviceAccount = getResources().openRawResource(R.raw.authentication_key);
-
-            // Authenticate with the service account
-            GoogleCredentials credentials = ServiceAccountCredentials.fromStream(serviceAccount);
-
-            // Use the credentials to initialize the client
-            LanguageServiceSettings settings = LanguageServiceSettings.newBuilder()
-                    .setCredentialsProvider(FixedCredentialsProvider.create(credentials))
-                    .build();
-
-            LanguageServiceClient languageServiceClient = LanguageServiceClient.create(settings);
-
-            // Now you can use languageServiceClient to access the API
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-     */
     private GoogleCredentials authenticateGoogleCloud() {
         try {
             // Load the service account key JSON file
@@ -253,7 +199,7 @@ public class MainActivity extends AppCompatActivity {
 
             JSONObject postData = new JSONObject();
             try {
-                postData.put("text", command); // Make sure the JSON key matches the expected key ("text")
+                postData.put("text", command);
             } catch (JSONException e) {
                 // Handle JSON exception
                 runOnUiThread(() -> callback.onFailure(e));
